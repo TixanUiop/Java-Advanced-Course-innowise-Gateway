b91818912e8fe17761902a29cbcca3140a5077c3
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-04-13 15:53:29 +0300

    added JwtAuthFilter

---
+++ src/main/java/com/innowise/gateway/security/JwtAuthFilter.java
@@ -0,0 +1,67 @@
+package com.innowise.gateway.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import com.innowise.gateway.util.JwtUtil;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.startsWith("/auth/login") ||
+                path.startsWith("/auth/register")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return unauthorized(exchange);
+        }
+
+        String token = authHeader.substring(7);
+
+        if (!jwtUtil.isValid(token)) {
+            return unauthorized(exchange);
+        }
+
+        String userId = jwtUtil.extractUserId(token);
+        String role = jwtUtil.extractRole(token);
+
+        ServerWebExchange modified = exchange.mutate()
+                .request(r -> r.headers(headers -> {
+                    headers.add("X-User-Id", userId);
+                    headers.add("X-User-Role", role);
+                }))
+                .build();
+
+        return chain.filter(modified);
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
